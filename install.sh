6acc78c390e4d61c596103817de613d6c17330bc
Hyan Mandian <hyanmandian@hotmail.com>
2018-06-26 05:37:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Setup
 apt-get install -y aptitude
 aptitude install git
+aptitude install curl
 aptitude install zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 chsh -s `which zsh`
@@ -19,20 +20,9 @@ wget http://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb
 dpkg -i vscode.deb
 
 # Node
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-nvm install 8.0.0
-nvm use 8.0.0
-nvm alias default 8.0.0
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+nvm install node
 
-# PHP
-curl -L -O https://github.com/phpbrew/phpbrew/raw/master/phpbrew
-chmod +x phpbrew
-sudo mv phpbrew /usr/bin/phpbrew
-phpbrew init
-phpbrew update
-phpbrew install 7.0.1
-
-# Ruby
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash
-rvm install 2.3.1
+# JVM
+curl -s "https://get.sdkman.io" | bash
+sdk install java
