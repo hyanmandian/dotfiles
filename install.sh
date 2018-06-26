51b73734b17ded1dad02af35d18d1ba252500420
Hyan Mandian <hyanmandian@hotmail.com>
2018-06-26 05:12:05 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # Setup
 apt-get install -y aptitude
+aptitude install git
 aptitude install zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 chsh -s `which zsh`
