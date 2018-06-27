e373576ba1bbeac93aa15a31ab35ffe72058cfd5
Hyan Mandian <hyanmandian@hotmail.com>
2018-06-27 00:26:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,23 +2,15 @@
 
 # Setup
 apt-get install -y aptitude
-aptitude install git
-aptitude install curl
-aptitude install zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-chsh -s `which zsh`
+aptitude install -y git curl zsh filezilla gconf2
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" && chsh -s `which zsh`
 
-# Tools
-aptitude install -y filezilla
-
-# Browsers
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-
-# Development
+# Apps
 snap install vscode --classic
 snap install intellij-idea-community --classic
 snap install spotify
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb && dpkg -i chrome.deb && rm chrome.deb
+wget https://github.com/meetfranz/franz/releases/download/v5.0.0-beta.18/franz_5.0.0-beta.18_amd64.deb -O franz.deb && dpkg -i franz.deb && rm franz.deb
 
 # Node
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
