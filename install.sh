417127ac08d5a17fb487a2dd56dae35c2737ad58
Hyan Mandian <hyanmandian@hotmail.com>
2018-06-30 17:27:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 apt-get install -y aptitude
 aptitude install -y git curl zsh filezilla gconf2 gconf-service libappindicator1 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" && chsh -s `which zsh`
-ln -s ~/Code/dotfiles/.zshrc .zshrc
 
 # Apps
 snap install vscode --classic
@@ -15,9 +14,11 @@ snap install postman
 wget https://github.com/meetfranz/franz/releases/download/v5.0.0-beta.18/franz_5.0.0-beta.18_amd64.deb -O franz.deb && dpkg -i franz.deb && rm franz.deb
 
 # Node
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | zsh
 nvm install node
 
 # JVM
-curl -s "https://get.sdkman.io"
+curl -s "https://get.sdkman.io" | zsh
 sdk install java
+
+ln -s ~/Code/dotfiles/.zshrc .zshrc
