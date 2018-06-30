7e504be1ff327f8d67431a2c2b4b7d05ddaffbcc
Hyan Mandian <hyanmandian@hotmail.com>
2018-06-30 00:51:24 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 snap install vscode --classic
 snap install intellij-idea-community --classic
 snap install spotify
+snap install chromium
 snap install postman
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb && dpkg -i chrome.deb && rm chrome.deb
 wget https://github.com/meetfranz/franz/releases/download/v5.0.0-beta.18/franz_5.0.0-beta.18_amd64.deb -O franz.deb && dpkg -i franz.deb && rm franz.deb
 
 # Node
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh
 nvm install node
 
 # JVM
-curl -s "https://get.sdkman.io" | bash
+curl -s "https://get.sdkman.io"
 sdk install java
