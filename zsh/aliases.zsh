820e9e9f3e82254f043e77c008dca212b64d96ee
Hyan Mandian <hyanmandian@hotmail.com>
2018-07-15 03:33:23 -0300

    Update

---
+++ zsh/aliases.zsh
@@ -0,0 +1,11 @@
+alias kill-containers="docker kill $(docker ps -q)"
+alias rm-containers="docker rm $(docker ps -a -q)"
+alias rm-images="docker rmi $(docker images -q)"
+alias kill-node="killall node"
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
+alias sudo="sudo -E "
+alias mv="mv -v"
+alias rm="rm -vi"
+alias cp="cp -v"
+alias untar='tar -xvf'
